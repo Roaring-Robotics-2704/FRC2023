9724b748ce00121fca0dc4367f10bb0758cc6b93
a-reid-code <110793689+a-reid-code@users.noreply.github.com>
2023-01-24 18:37:57 -0600

    added correct robotcontainer code
    forgot to put this in the initial commit

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,11 +6,23 @@ package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
+//import frc.robot.commands.DriveToTargetDistance;
 import frc.robot.commands.ExampleCommand;
 import frc.robot.subsystems.ExampleSubsystem;
+import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.Vision;
+
+//import frc.robot.commands.DriveToTargetDistance;
+//import frc.robot.commands.RotateToTargetYaw;
+import frc.robot.commands.DefaultDrive;
+
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -21,14 +33,21 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
+  public static Drivetrain m_drivetrain = new Drivetrain();
+  public static Vision m_vision = new Vision();
+  
+  
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController m_driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  public static XboxController m_xbox =
+      new XboxController(0);
+  public static JoystickButton distanceButton = new JoystickButton(m_xbox, 4);
+  public static JoystickButton rotateButton = new JoystickButton(m_xbox, 1);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
     // Configure the trigger bindings
+    m_drivetrain.setDefaultCommand(new DefaultDrive(m_drivetrain, m_vision, m_xbox));
     configureBindings();
   }
 
@@ -43,12 +62,16 @@ public class RobotContainer {
    */
   private void configureBindings() {
     // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_exampleSubsystem::exampleCondition)
-        .onTrue(new ExampleCommand(m_exampleSubsystem));
+    //new Trigger(m_exampleSubsystem::exampleCondition)
+        //.onTrue(new ExampleCommand(m_exampleSubsystem));
 
     // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
     // cancelling on release.
-    m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+    //m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+    
+   // distanceButton.whileTrue(new DriveToTargetDistance(m_drivetrain, m_vision)).whileFalse(new DefaultDrive(m_drivetrain));
+  //  rotateButton.whileTrue(new RotateToTargetYaw());
+
   }
 
   /**
