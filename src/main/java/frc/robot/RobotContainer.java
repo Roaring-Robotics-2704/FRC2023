b60f4eef38522516855e666a6a4bc857628fed5e
annellabellot <artemis011235@gmail.com>
2023-02-18 14:30:07 -0600

    Changes to Prepare for Main

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,13 +4,10 @@
 
 package frc.robot;
 
-import frc.robot.commands.Autos;
 import frc.robot.commands.ControlIntake;
-import frc.robot.commands.ExampleCommand;
 import frc.robot.commands.MoveArm;
 import frc.robot.subsystems.EverybotArm;
 import frc.robot.subsystems.EverybotIntake;
-import frc.robot.subsystems.ExampleSubsystem;
 import frc.robot.Constants.OperatorConstants;
 
 import edu.wpi.first.wpilibj2.command.Command;
@@ -27,7 +24,6 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
   private EverybotArm s_everybotArmSubsystem = new EverybotArm();
   private EverybotIntake s_everybotIntakeSubsystem = new EverybotIntake();
  
@@ -53,20 +49,12 @@ public class RobotContainer {
    */
   private void configureBindings() {
     // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
-    new Trigger(m_exampleSubsystem::exampleCondition).onTrue(new ExampleCommand(m_exampleSubsystem));
+    //new Trigger(m_exampleSubsystem::exampleCondition).onTrue(new ExampleCommand(m_exampleSubsystem));
 
     // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
     // cancelling on release.
     // m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
   }
 
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
-  public Command getAutonomousCommand() {
-    // An example command will be run in autonomous
-    return Autos.exampleAuto(m_exampleSubsystem);
-  }
+ 
 }
