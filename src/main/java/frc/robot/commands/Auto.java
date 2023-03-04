0905c4ac5b3bdf320cee68348836ad1218cd4c69
cordelia34 <107320320+cordelia34@users.noreply.github.com>
2023-03-04 12:09:01 -0600

    Improvements

--- src/main/java/frc/robot/commands/Auto.java
+++ src/main/java/frc/robot/commands/Auto.java
@@ -1,73 +1,68 @@
 package frc.robot.commands;
 
-import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj.ADIS16470_IMU;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.Constants;
 import frc.robot.RobotContainer;
+
 
 public class Auto extends CommandBase{
    public Auto() {
     addRequirements(RobotContainer.m_Drivetrain);
    } 
-   ADIS16470_IMU gyro = RobotContainer.m_imu;
-   private void moveAuto(double y,double x,double z) {
-    RobotContainer.m_Drivetrain.driveCartesian(y,x,z,-gyro.getAngle());   
-    }
+  
+private void moveAuto(double y,double x,double z) {
+RobotContainer.m_Drivetrain.driveCartesian(y,x,z,-RobotContainer.m_imu.getAngle()); 
+}
 Timer autoTime = new Timer();
 public int mode;
 
 @Override
 public void initialize(){
     autoTime.reset();
-    gyro.calibrate();
-    gyro.reset();
     
 }
 double outputz;
-PIDController align = new PIDController(Constants.zpid.p,Constants.zpid.i,Constants.zpid.d);
 @Override
 public void execute() {
     autoTime.start();
     SmartDashboard.putNumber("autoTime",autoTime.get());
     if (mode == 1) {//square
-        while (autoTime.get() <= 1){//backwards
-            moveAuto(-0.3,0,0);
+        while (autoTime.get() <= 2.06){//backwards
+            moveAuto(-0.6,0,0);
         }
         
         while ( autoTime.get() <= 2){//right
-            moveAuto(0,0.3,0);
+          moveAuto(0,0.3,0);
         }
         
-        while  ( autoTime.get() <= 3){//forwards
+         while  ( autoTime.get() <= 3){//forwards
             moveAuto(0.3,0,0);
         }
         while  ( autoTime.get() <= 4){//left
             moveAuto(0,-0.3,0);
         }
-    }          
-    else if (mode == 2) {//backwards
-        while ( autoTime.get() <= 1.6){
-            moveAuto(-0.3,0,0);
-            
-        
+    }    
+     else if (mode == 2) {//backwards
+        while ( autoTime.get() <= 2.57){
+            moveAuto(0.8,0,0);
         }
+        
     }
     else if (mode == 3) {//back,sideways,back
-        while (autoTime.get()<= 5.5){
-            moveAuto(0.5,0,0);
+        while (autoTime.get()<= 2){
+            moveAuto(-0.3,0,0);
         }
-        while (autoTime.get()<=10){
-            moveAuto(0,-0.5,0);
+        while (autoTime.get()<=3){
+            moveAuto(0,-0.3,0);
         }
-        while(autoTime.get()<= 15){
-            moveAuto(-0.5,0,0);
+        while(autoTime.get()<= 4){
+            moveAuto(-0.3,0,0);
         }
     
     }
-    else if (mode == 4) {//spinning square
+     else if (mode == 4) {//spinning square
         while (autoTime.get()<=1) {//back
            moveAuto(-0.3,0,0.3);
         }
@@ -80,25 +75,66 @@ public void execute() {
         while (autoTime.get()<=4) {//left
             moveAuto(0,-0.3,0.3);
         }
-        while (gyro.getAngle() != 0){
-            moveAuto(0,0,align.calculate(gyro.getAngle(),0)/180);
-            
+    }
+    else if (mode==5){
+        while (autoTime.get() <=1){
+            moveAuto(0.3, 0, 0);
+        }
+        while (autoTime.get() <= 2){
+            moveAuto(0,0.3,0);
+        }
+        while(autoTime.get() <=3 ){
+            moveAuto(0.3, 0,  0 );
         }
     }
-    else if (mode == 5) {
-        while (autoTime.get() <= 1.7) {
-            moveAuto(0,0,0.3);}
-        while (autoTime.get()>=1.7) 
-            while (gyro.getAngle() != 0) {
-            moveAuto(0,0,align.calculate(gyro.getAngle(),0));
+    else if (mode == 6){// on and off 
+        while(autoTime.get()<= 3){// fowards over the chrage station 
+            moveAuto(0.8, 0, 0);// speed needs to be at least 0.8 for the robot to climb the charge station due to the arm 
+        }
+        while (autoTime.get()<= 4.55){ // backwards onto the charge station until center 
+            moveAuto(-0.8, 0, 0);
         }
     }
-}
+    else if (mode == 7){// right side balance 
+        while(autoTime.get()<=3){// forwards 
+            moveAuto(0.8,0,0);
+        }
+        while (autoTime.get()<=4.5){// left 
+            moveAuto(0, -0.8, 0);
+        }
+        while (autoTime.get()<=5){// backwards 
+            moveAuto(-0.8, 0,0 );
+        }
+        }
+    else if (mode == 8){// left side balance 
+        while(autoTime.get()<=3){// forwards 
+                moveAuto(0.8,0,0);
+            }
+        while (autoTime.get()<=4.5){// right  
+                moveAuto(0, 0.8, 0);
+            }
+        while (autoTime.get()<=4){// backwards 
+                moveAuto(-0.8, 0,0 );
+         }
+         }
+    else if (mode == 9){
+        while(autoTime.get()<= 3){
+            moveAuto(0, 0.8, 0);
+        }
+        while(autoTime.get()<= 3.5){
+            moveAuto(0, 0, 0);
+        }
+    }
+    }
+    
+
 
 @Override
 public boolean isFinished(){
     return false;
 
     
-}}
+}
+}
+
 
