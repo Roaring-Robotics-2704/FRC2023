55ee3c418dd2a991aa66c7b71c43e21c03b773a2
mmarchjr <mmarchiniakjr@hotmail.com>
2023-03-10 21:04:03 +0000

    hey, look over there!

--- src/main/java/frc/robot/Mechanum/mechanumConf.java
+++ src/main/java/frc/robot/Mechanum/mechanumConf.java
@@ -3,20 +3,20 @@ package frc.robot.Mechanum;
 public class mechanumConf extends mecanum {
     public static class wheelLocations {
         //front left wheel offset to center of bot in meters
-        public static double frontLeftWheelX = 0.381; //+ is to front of robot
-        public static double frontLeftWheelY = 0.381; //+ is to left of robot
+        public static double frontLeftWheelX = 0.2286; //+ is to front of robot
+        public static double frontLeftWheelY = 0.2286; //+ is to left of robot
         
         //left wheel offset to center of bot in meters
-        public static double frontRightWheelX = 0.381; //+ is to front of robot
-        public static double frontRightWheelY = -0.381; //+ is to left of robot
+        public static double frontRightWheelX = 0.2286; //+ is to front of robot
+        public static double frontRightWheelY = -0.2286; //+ is to left of robot
        
         //front left wheel offset to center of bot in meters
-        public static double backLeftWheelX = -0.381; //+ is to front of robot
-        public static double backLeftWheelY = 0.381; //+ is to left of robot
+        public static double backLeftWheelX = -0.2286; //+ is to front of robot
+        public static double backLeftWheelY = 0.2286; //+ is to left of robot
         
         //left wheel offset to center of bot in meters
-        public static double backRightWheelX = -0.381; //+ is to front of robot
-        public static double backRightWheelY = -0.381; //+ is to left of robot
+        public static double backRightWheelX = -0.2286; //+ is to front of robot
+        public static double backRightWheelY = -0.2286; //+ is to left of robot
     } 
     public static class motorPorts {
         //motor ports on can bus
